08d72a77baab25f5b55fc5267932c3280f90a39e
Xalalau Xubilozo <xalalau@outlook.com>
2021-04-13 02:08:51 -0300

    Set to rev 24.2

--- lua/xmh/client/xmh_cl.lua
+++ lua/xmh/client/xmh_cl.lua
@@ -1,6 +1,6 @@
 --[[
    \   XALA'S MOVIE HELPER
- =3 ]]  Revision = "XMH.Rev.24.1 - 12/04/2021 (dd/mm/yyyy)" --[[
+ =3 ]]  Revision = "XMH.Rev.24.2 - 13/04/2021 (dd/mm/yyyy)" --[[
  =o |   License: MIT
    /   Created by: Xalalau Xubilozo
   |

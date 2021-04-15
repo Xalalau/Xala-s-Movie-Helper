54cdc32a79282eb3618eaa5c8cf923286e6bffdd
Xalalau Xubilozo <xalalau@outlook.com>
2021-04-15 19:58:32 -0300

    Finish version 25

--- lua/xmh/client/xmh_cl.lua
+++ lua/xmh/client/xmh_cl.lua
@@ -1,6 +1,6 @@
 --[[
    \   XALA'S MOVIE HELPER
- =3 ]]  Revision = "XMH.Rev.24.4 - 15/04/2021 (dd/mm/yyyy)" --[[
+ =3 ]]  Revision = "XMH.Rev.25 - 15/04/2021 (dd/mm/yyyy)" --[[
  =o |   License: MIT
    /   Created by: Xalalau Xubilozo
   |
